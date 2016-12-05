af2f692f6703575b0dc0b2e77acd58670a00282e
Aman <amsingh714@gmail.com>
2016-12-05 16:57:40 -0500

    first commit

---
+++ git-and-github-practice-two/Car.swift
@@ -0,0 +1,28 @@
+//
+//  Car.swift
+//  git-and-github-practice-two
+//
+//  Created by Aman Singh on 12/5/16.
+//  Copyright © 2016 Aman Singh. All rights reserved.
+//
+
+import UIKit
+
+    class Car {
+        var description: String
+        var numberOfWheels: Int
+        var maxSpeed: Float
+        
+        
+        init(description: String, numberOfWheels: Int, maxSpeed: Float) {
+            self.description = description
+            self.numberOfWheels = numberOfWheels
+            self.maxSpeed = maxSpeed
+        }
+    }
+    
+    class Garage {
+        
+    }
+
+
